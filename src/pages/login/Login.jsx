8e57e276463647e65b87ef27be5682c902e61154
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-03-23 21:40:26 +0530

    Updated Login connection with Localhost API

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdInventory } from "react-icons/md";
 import Footer from "../global/Footer";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { API } from "../../api/api";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [datas, setDatas] = useState([]);
+  const navigate=useNavigate()
+
+  const handleLogin = async () => {
+    try {
+      const loginData = {
+        email,
+        password,
+      };
+      console.log(loginData);
+      const response = await axios.post(`${API}login`, loginData);
+      await setDatas([...datas, loginData]);
+      localStorage.setItem("login", "true");
+      response.data.result.token &&
+        localStorage.setItem("token", response.data.result.token || "");
+        if( localStorage.getItem("login", "true")){
+          navigate('/dashboard')
+        }
+     
+    } catch (err) {
+      console.log("Error", err.message);
+    }
+  };
+
+  const handleEmailadress = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <main className="login ">
       <div className="flex  flex-col justify-center items-center  h-screen">
@@ -20,14 +56,19 @@ export default function Login() {
             <input
               type="text"
               placeholder="Email address"
+              onChange={(e) => handleEmailadress(e)}
               className="m-2 px-2 py-1 border bg-slate-100 rounded-md"
             />
             <input
               type="text"
               placeholder="Password"
+              onChange={(e) => handlePassword(e)}
               className="m-2 px-2 py-1 border bg-slate-100 rounded-md"
             />
-            <button className="m-2 p-2 border rounded-lg bg-buttonColor text-white font-semibold cursor-pointer">
+            <button
+              className="m-2 p-2 border rounded-lg bg-buttonColor text-white font-semibold cursor-pointer"
+              onClick={() => handleLogin()}
+            >
               Login
             </button>
             <p className="text-base font-light px-2 my-1">
