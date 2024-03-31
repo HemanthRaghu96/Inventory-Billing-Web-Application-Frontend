97774c5a750b0be604c2cc6ac8aeb44b9d425f1b
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-03-31 18:08:07 +0530

    Created and Updated Invoice UI with API

--- src/pages/Sales/Invoices/AddInvoices.jsx
+++ src/pages/Sales/Invoices/AddInvoices.jsx
@@ -1,7 +1,228 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { HiMiniXMark } from "react-icons/hi2";
+import { Link, useNavigate } from "react-router-dom";
+import { API } from "../../../api/api";
+import axios from "axios";
+import CustomerDropdown from "../../../components/CustomerDropdown";
 
 export default function AddInvoices() {
+  const navigate = useNavigate();
+  const [customername, setCustomerName] = useState("");
+  const [ordernumber, setOrderNumber] = useState("");
+  const [invoice, setInvoice] = useState("");
+  const [invoicedate, setInvoicedate] = useState("");
+  const [duedate, setDuedate] = useState("");
+  const [items, setItems] = useState([]);
+  const [total, setTotal] = useState("");
+  const [shippingcharges, setShippingCharges] = useState("");
+  const [customernote, setCustomerNote] = useState("");
+console.log(items)
+  const handleSave = async () => {
+    const invoiceData = {
+      customername,
+      ordernumber,
+      invoice,
+      invoicedate,
+      duedate,
+      items,
+      shippingcharges,
+      customernote,
+    };
+    try {
+      const response = await axios.post(`${API}addinvoice`, invoiceData);
+      console.log(response.data); // handle response as needed
+      navigate("/invoices"); // navigate to sales orders page on successful save
+    } catch (error) {
+      console.error("Error adding sales order:", error);
+      // Handle error state or display error message to the user
+    }
+  };
+  const addItem = () => {
+    const newItem = { name: "", quantity: 1, price: 0 };
+    setItems([...items, newItem]);
+  };
+  const deleteItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+  // Function to handle changes in item details
+  const handleItemChange = (index, field, value) => {
+    const updatedItems = [...items];
+    updatedItems[index][field] = value;
+    setItems(updatedItems);
+  };
+  useEffect(() => {
+    let calculatedTotal = 0;
+    items.forEach((item) => {
+      calculatedTotal += item.price;
+    });
+    setTotal(calculatedTotal);
+  }, [items]);
   return (
-    <div>AddInvoices</div>
-  )
+    <section className="ml-14 mt-16 md:ml-56 h-full overflow-y-auto">
+      <div className="flex justify-between mr-5 md:mr-10 lg:mr-20">
+        <h1 className="font-semibold text-xl">New Invoice</h1>
+        <Link to="/invoices">
+          <HiMiniXMark className="mr-1 size-7 text-red-500" />
+        </Link>
+      </div>
+      {/* Sales Order Form */}
+      <div className="flex justify-start">
+        <div className="mt-5 w-[420px]">
+          <div className="flex justify-between my-4">
+            <h1>Customer Name</h1>
+            <CustomerDropdown
+              customername={customername}
+              setCustomerName={setCustomerName}
+            />
+          </div>
+          <div className="flex justify-between my-4">
+            <h1>Invoice Number</h1>
+            <input
+              type="text"
+              className="border-2 rounded-md px-2 h-8"
+              onChange={(e) => setInvoice(e.target.value)}
+              value={invoice}
+            />
+          </div>
+          <div className="flex justify-between my-4">
+            <h1>Sales Order Number</h1>
+            <input
+              type="text"
+              className="border-2 rounded-md px-2 h-8"
+              onChange={(e) => setOrderNumber(e.target.value)}
+              value={ordernumber}
+            />
+          </div>
+          <div className="flex justify-between my-4">
+            <h1> Invoice Date</h1>
+            <input
+              type="text"
+              className="border-2 rounded-md px-2 h-8"
+              onChange={(e) => setInvoicedate(e.target.value)}
+              value={invoicedate}
+            />
+          </div>
+          <div className="flex justify-between my-4">
+            <h1>Due Date</h1>
+            <input
+              type="text"
+              className="border-2 rounded-md px-2 h-8"
+              onChange={(e) => setDuedate(e.target.value)}
+              value={duedate}
+            />
+          </div>
+        </div>
+      </div>
+      {/* Item Table */}
+      <div className="my-4">
+        <h1 className="font-semibold text-lg">Item Table</h1>
+        <table className="w-full mt-4">
+          <thead className="bg-gray-100">
+            <tr >
+              <th>Item Name</th>
+              <th>Quantity</th>
+              <th>Discount</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr key={index}>
+                <td>
+                  <input
+                    type="text"
+                    value={item.name}
+                    onChange={(e) =>
+                      handleItemChange(index, "name", e.target.value)
+                    }
+                    className="border-2 rounded-md px-2 h-8 w-full"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    value={item.quantity}
+                    onChange={(e) =>
+                      handleItemChange(
+                        index,
+                        "quantity",
+                        parseInt(e.target.value)
+                      )
+                    }
+                    className="border-2 rounded-md px-2 h-8 w-full"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    value={item.discount}
+                    onChange={(e) =>
+                      handleItemChange(
+                        index,
+                        "discount",
+                        parseInt(e.target.value)
+                      )
+                    }
+                    className="border-2 rounded-md px-2 h-8 w-full"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    value={item.price}
+                    onChange={(e) =>
+                      handleItemChange(
+                        index,
+                        "price",
+                        parseFloat(e.target.value)
+                      )
+                    }
+                    className="border-2 rounded-md px-2 h-8 w-full"
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => deleteItem(index)}
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button
+          className="mt-3 border rounded-lg bg-buttonColor text-white px-2 py-1 mr-3"
+          onClick={addItem}
+        >
+          Add Item
+        </button>
+      </div>
+<div className="">
+<h1 className="font-semibold text-lg">Sub Total</h1>
+<h1 className="flex">Shipping Charges <span><input type="text" className="border-2 rounded-md px-2 h-8 ml-10" onChange={(e)=>setShippingCharges(e.target.value)}/></span></h1>
+<h1  className="flex mt-2 font-semibold">Total <span className="ml-32 font-semibold">{total-shippingcharges}</span></h1>
+
+
+</div>
+      {/* Buttons for Save and Cancel */}
+      <div className="flex my-4">
+        <button
+          className="border rounded-lg bg-buttonColor text-white px-2 py-1 mr-3"
+          onClick={handleSave}
+        >
+          Save
+        </button>
+        <button
+          className="border rounded-lg px-2 py-1"
+          onClick={() => navigate("/invoices")}
+        >
+          Cancel
+        </button>
+      </div>
+    </section>
+  );
 }
--- src/pages/Sales/Invoices/ViewSingleInvoices.jsx
+++ src/pages/Sales/Invoices/ViewSingleInvoices.jsx
@@ -1,7 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { HiMiniXMark } from "react-icons/hi2";
+import { Link } from "react-router-dom";
+import { MdOutlineEdit } from "react-icons/md";
+import { MdDeleteOutline } from "react-icons/md";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { API } from "../../../api/api";
 
 export default function ViewSingleInvoices() {
+  const [data, setData] = useState([]);
+  const { invoicesId } = useParams();
+  const column = ["NAME", "QUANTITY", "PRICE"];
+  useEffect(() => {
+    fetchSelecteData();
+  }, []);
+  const fetchSelecteData = async () => {
+    const response = await axios.get(
+      `${API}getselectedinvoice/${invoicesId}`
+    );
+    console.log(response.data.selectedInvoices[0]);
+    setData(response.data.selectedInvoices[0]);
+  };
+  const handleDelete = async () => {
+    await axios.delete(`${API}deleteinvoice/${invoicesId}`);
+  };
   return (
-    <div>ViewSingleInvoices</div>
-  )
+    <section className="ml-14 mt-16  md:ml-56 h-screen">
+      <div className="flex justify-between mr-5 md:mr-10 lg:mr-20">
+        <h1 className="font-semibold text-xl">Invoice Details</h1>
+        <div className="flex">
+          <Link to={`/invoices/invoicesitem/${invoicesId}`}>
+            <button className="px-2 flex items-center bg-buttonColor text-white rounded-md mx-2">
+              <MdOutlineEdit className="mr-1" />
+              Edit
+            </button>
+          </Link>
+          <Link to={"/invoices"}>
+            <button
+              className="px-2 flex items-center bg-buttonColor text-white rounded-md mx-2"
+              onClick={() => handleDelete()}
+            >
+              <MdDeleteOutline className="mr-1" />
+              Delete
+            </button>
+          </Link>
+          <Link to={"/invoices"}>
+            <HiMiniXMark className="mr-1 ml-20 size-7 text-red-500" />
+          </Link>
+        </div>
+      </div>
+      <div className="flex ">
+        <div className="py-1 my-1 w-[200px]">
+          <h1 className="my-1 text-lg">Customer Name</h1>
+          <h1 className="my-1 text-lg">Invoice</h1>
+          <h1 className="my-1 text-lg">Sales Order Number</h1>
+          <h1 className="my-1 text-lg">Invoice Date</h1>
+          <h1 className="my-1 text-lg">Due Date</h1>
+        </div>
+        <div className="py-1 my-1  w-40">
+          <h1 className="my-1 text-lg">
+            {data.customername == null ? "null" : data.customername}
+          </h1>
+          <h1 className="my-1 text-lg">{data.invoice}</h1>
+          <h1 className="my-1 text-lg">{data.ordernumber}</h1>
+          <h1 className="my-1 text-lg">
+            {data.invoicedate == null ? "null" : data.invoicedate}
+          </h1>
+          <h1 className="my-1 text-lg">
+            {data.duedate == null ? "null" : data.duedate}
+          </h1>
+        </div>
+      </div>
+      <div className="overflow-x-auto w-[600px]">
+      <h1 className="font-semibold text-xl mt-4 mb-2">Items Table</h1>
+        <table className="table-auto min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              {column.map((data, i) => (
+                <th
+                  key={i}
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  {data}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {data.items ? (
+              data.items.map((record, i) => (
+                <tr key={i}>
+                  <td className="px-6 py-4 whitespace-nowrap flex items-center">
+                    
+                    {record.name}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap">{record.quantity}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{record.price}</td>
+                  
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="5">No items available</td>
+              </tr>
+            )}
+              
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
 }
